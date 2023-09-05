1c78455cfad6a7d7fb31f1f5671cf88315496e74
ajenin <jenin278@gmail.com>
2023-09-05 23:51:54 +0530

    It gives the priorty to the arithmetic operations

---
+++ src/operator_precedence.java
@@ -0,0 +1,8 @@
+public class operator_precedence {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 7;
+        int c=a*b+a;
+        System.out.println(c);
+    }
+}
