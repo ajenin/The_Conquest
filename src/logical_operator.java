2f48eaf9904550cd56f61c6a6a966be3d72a8304
ajenin <jenin278@gmail.com>
2023-09-05 23:31:01 +0530

    Added "or"

--- src/logical_operator.java
+++ src/logical_operator.java
@@ -4,5 +4,10 @@ public class logical_operator {
         boolean d=false;
 
         System.out.println(c || d);
+
+        boolean e=true;
+        boolean m=false;
+
+        System.out.println(e && m);
     }
 }
